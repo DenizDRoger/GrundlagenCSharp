8ed3ad2e2688870bf651d5a23553f5b5e404df48
Deniz <kartal.deniiz@yahoo.com>
2022-04-08 12:25:51 +0200

    zuhause bessern

--- Methoden_uebung_hotel/Program.cs
+++ Methoden_uebung_hotel/Program.cs
@@ -6,88 +6,53 @@ namespace Methoden_uebung_hotel
     {
         static void Main(string[] args)
         {
-            bool richtig = false;
-            bool richtig2 = false;
-            string nachname;
-            short zimmernummer;
-            string[] zimmernummern = { "45", "23", "3", "32" };
-            string[] vornamen = { "John", "Paul", "Geroge", "Ringo" };
-            string[] nachnamen = { "Lennon", "McCartney", "Harrison", "Starr" };
-            short[] übernachtungen = { 1, 4, 12, 3 };
-            double[] rechnungsbetrag = {82.46, 329.84, 989.52, 247.38};
+            short[] zimmernummer = { 45, 23, 3, 32 };
+            string[] vorname = { "John", "Paul", "George", "Ringo" };
+            string[] nachname = { "Lennon", "McCartney", "Harrison", "Starr" };
+            short[] übernachtung = { 1, 4, 12, 3 };
+            double[] rechnungsbetrag = { 82.46, 329.84, 989.52, 247.38 };
 
-            Console.WriteLine("Bitte geben Sie ihren Nachnamen oder ihre Zimmernummer ein:");
-           
-            
-            nachname = Console.ReadLine();
-            if (Int16.TryParse(nachname, out zimmernummer))
+            do
             {
-                richtig2 = GetData(richtig);
-            }
+                Console.WriteLine("Geben Sie ihre Daten ein");
+                string eingabe_str = Console.ReadLine();
+                short eingabe_short;
 
-            else
-            {
+                bool flag = short.TryParse(eingabe_str, out eingabe_short);
 
-            }
+                if (flag == true)
+                {
+                    Console.WriteLine(GetData(eingabe_short, zimmernummer, vorname, nachname, übernachtung, rechnungsbetrag));
+                }
+                else
+                {
+                    
+                    Console.WriteLine(GetData(eingabe_str, zimmernummer, vorname, nachname, übernachtung, rechnungsbetrag));
+                }
 
-
-
-            Console.WriteLine();
-
-            
-
-
-
-
-
-
+            } while (true);
         }
-        static bool GetData(string nachname, string[] nachnamen, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig)
+        static (bool, short, string, string, short, double) GetData(short diff, short[] zn, string[] vn, string[] nn, short[] ue, double[] rb)
         {
-            if (eingabe == nachnamen)
-            {
-                richtig = true;
-            }
-
-            else if (eingabe == zimmernummern)
-            {
-                richtig = true;
-            }
-
-            else
-            {
-                ausgabe = "Ihre Eingabe ist falsch.";
-                richtig = false;
-            }
 
             
+            if (eingabe == zn)
 
-            return richtig;
+
+
+            return (true, 0, "", "", 0, 0.0);
         }
-
-        static bool GetData(short zimmernummer, short[]zimmernummern, string[] vornamen, short[] übernachtungen, double[] rechnungsbetrag, bool richtig)
+        static (bool, short, string, string, short, double) GetData(string diff, short[] zn, string[] vn, string[] nn, short[] ue, double[] rb)
         {
-            if (eingabe == nachnamen)
-            {
-                ausgabe = "hallo";
-                richtig = true;
-            }
-
-            else if (eingabe == zimmernummern)
-            {
-                ausgabe = "hallo";
-                richtig = true;
-            }
-
-            else
-            {
-                ausgabe = "Ihre Eingabe ist falsch.";
-                richtig = false;
-            }
 
 
 
-            return richtig;
+
+            return (true, 0, "", "", 0, 0.0);
         }
+
+
+
+
     }
 }
